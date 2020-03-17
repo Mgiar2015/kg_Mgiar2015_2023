6e6aadc98edbfb4418cf235b10578ae404bdfd39
Michael G <mgiar2015@Michaels-MBP-2.fios-router.home>
2020-03-17 19:39:03 -0400

    Support added for command line arguments. Hard coded test removed. Some new comments added.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-#The definition of a "one-to-one character mapping" is somewhat unclear.
-#I am assuming this assignment does not allow for the asking of questions,
-#therefore for my solution I assume that a "one-to-one character mapping" implies
-# String s1 can be transformed into string s2 by the direct substition of the
+import sys
+# The definition of a "one-to-one character mapping" is somewhat unclear.
+# I am assuming this assignment does not allow for the asking of questions,
+# therefore for my solution I assume that a "one-to-one character mapping" implies
+# string s1 can be transformed into string s2 by the direct substition of the
 # characters in s1 with their corresponding character in s2
+# (Each unique character in s1 can only be assosiated to a single character in s2)
 def evaluate(s1,s2):
     char_map = {}
     if len(s1) != len(s2):
@@ -16,4 +18,6 @@ def evaluate(s1,s2):
             pass
     return True
 
-print(evaluate("abbb","cdcd"))
+# I conver the output to a string and then print in lowercase so the output
+# matches the example provided
+print(str(evaluate(sys.argv[1],sys.argv[2])).lower())
