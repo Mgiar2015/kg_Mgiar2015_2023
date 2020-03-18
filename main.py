fc8a10053a5d7a113de6566b94bbe0235cc13b8e
Michael G <mgiar2015@Michaels-MBP-2.fios-router.home>
2020-03-17 20:27:19 -0400

    Adjustment made to comment for further clarifcation of scope of solution

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import sys
 # characters in s1 with their corresponding character in s2
 # (Each unique character in s1 can only be assosiated to a single character in s2)
 # I am also assuming the two string must be of the same length, based on the provivded examples
+# For examples such as where s1 is aaabc and s2 is deeef this solution will return False
+# because I am working under the assumption that the order of chatacters must be maintained.
 def evaluate(s1,s2):
     char_map = {}
     if len(s1) != len(s2):
